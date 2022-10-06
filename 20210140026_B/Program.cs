e2fa06e3673d31e58194930ad813eeff94ee0b95
DavitPH <davitph0@gmail.com>
2022-10-06 17:42:12 +0700

    Commit ke-3

--- 20210140026_B/Program.cs
+++ 20210140026_B/Program.cs
@@ -45,6 +45,23 @@ namespace Bubble_Sort
             Console.WriteLine("");
         }
 
+        public void BubbleSortArray()
+        {
+            for(int i = 1; i < n; i++)
+            {
+                for(int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+
+                    }
+                }
+            }
+        }
     }
     class Program
     {
