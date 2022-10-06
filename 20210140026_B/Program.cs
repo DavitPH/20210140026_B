ab80b89005fd1b2638de15f8c74b2f96b07845a5
WINDOWS 10 <WINDOWS 10@MSI>
2022-10-06 17:10:35 +0700

    Add project files.

---
+++ 20210140026_B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20210140026_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
